5f7869c0931b604bedfd26cfbf49cb0337645aaa
andrii.kovalenkov <andrii.kovalenkov@decsoft.com.pl>
2021-04-19 11:13:05 +0200

    1) Dodane no-catche, static-location w application.properties 2) Utworzone statyczne pliki static/css, static/js, static/img 3) Zmieniony blocks/header - został dodany fragment:head uzupełniający tag <head> na każdej stronie. 4) Dodany test class "WebConfig" prośba nie usuwać ten class

---
+++ src/main/java/com/testweb/TestBlog/WebConfig.java
@@ -0,0 +1,31 @@
+/*
+ * Не удаляй этот файл
+ */
+
+//package com.testweb.TestBlog;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+//
+//@Configuration
+//@EnableWebMvc
+//public class WebConfig extends WebMvcConfigurerAdapter {
+//
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler(
+//                "/templates/**",
+//                "/img/**",
+//                "/css/**",
+//                "/js/**")
+//                .addResourceLocations(
+////                        "classpath:/META-INF/resources/templates/",
+//                        "classpath:/resources/templates/",
+//                        "classpath:/static/css/",
+//                        "classpath:/static/img/",
+//                        "classpath:/static/js/");
+//    }
+//
+//}
